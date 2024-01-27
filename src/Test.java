901038c3a764715dc8c23d0b72d042fca3682fab
HirenpatelD <147208292+HirenpatelD@users.noreply.github.com>
2024-01-27 15:09:40 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Patel");
+        System.out.println("My name is Patel, How are you");
     }
 }
